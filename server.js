031420018809504749cd4e68da8f847f4d0cd760
Davi Macedo <dvmacedo14@gmail.com>
2026-03-03 19:20:01 -0300

    fix: remove insecure exec usage

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
-const { exec } = require("child_process");
 
-exec("ls -la"); // insecure test
+
+
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
